ce83bd58b25a51e20905a0da3095c1b3dc9cf764
00hongjun <00hongjun@gmai.com>
2021-05-10 00:40:40 +0900

    <refactor> 컨트롤러 이름 오타 수정

--- src/main/java/com/github/springsecuritystudy/account/AccountController.java
+++ src/main/java/com/github/springsecuritystudy/account/AccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class AccountControler {
+public class AccountController {
 
     private final AccountRepository accountRepository;
     private final AccountService accountService;
