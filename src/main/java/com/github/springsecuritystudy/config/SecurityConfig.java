8e11608b1c1a78c83764b2ccfc788a4b424fd002
00hongjun <00hongjun@gmail.com>
2021-05-31 16:58:48 +0900

    <feat> 시큐리티 ignoring() 설정

--- src/main/java/com/github/springsecuritystudy/config/SecurityConfig.java
+++ src/main/java/com/github/springsecuritystudy/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.springsecuritystudy.config;
 
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
@@ -10,6 +11,7 @@ import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -43,6 +45,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         handler.setRoleHierarchy(roleHierarchy);
 
         return handler;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().mvcMatchers("/favicon.ico");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Override
